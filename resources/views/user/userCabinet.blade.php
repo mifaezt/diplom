994a699e87f1a43dedc0d8de6338c3806d7efa59
Igor Ilin <mifaezt@google.com>
2025-02-28 18:05:25 +0300

    miniUpdate1.1

--- resources/views/user/userCabinet.blade.php
+++ resources/views/user/userCabinet.blade.php
@@ -7,12 +7,22 @@
 
 
 <div class="container">
-    <div class="form-container">
+<section class="profile-section">
         <h2>Личный кабинет</h2>
         <h3> Добро пожаловать {{auth()->user()->name}}</h3>
         <a href="{{route('logout')}}">Выйти</a>
-       
+        <h2>Мой профиль</h2>
+            <p><strong>Имя:</strong> Иван Иванов</p>
+            <p><strong>Email:</strong> ivan@example.com</p>
+            <p><strong>Телефон:</strong> +7 (123) 456-78-90</p>
+            <button>Редактировать профиль</button>
+        </section>
+
+    
     </div>
+    <footer>
+        <p>&copy; 2025 Jarviranta. Все права защищены.</p>
+    </footer>
 </div>
 
 @endsection
